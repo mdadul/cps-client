08d4f7cf63fa912352d4881c0889f7749a3d65c3
Emdadul Islam <58410798+mdadul@users.noreply.github.com>
2022-12-07 14:04:38 +0000

    add course Form

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,47 +1,93 @@
 import React from 'react'
+import {useRef} from 'react'
 import Form from './Form'
 
 import TextInput from './TextInput'
 
 export default function CourseForm() {
+    const imageRef = useRef();
+    const titleRef = useRef();
+    const priceRef = useRef();
+    const totalClassRef = useRef();
+    const totalLectureRef = useRef();
+    const totalTimeRef = useRef();
+    const totalStudentRef = useRef();
+
+
+    const handleCourseForm=e=>{
+        e.preventDefault();
+
+        const image = imageRef.current.value;
+        const title = titleRef.current.value;
+        const price = priceRef.current.value;
+        const totalClass = totalClassRef.current.value;
+        const totalLecture = totalLectureRef.current.value;
+        const totalTime = totalTimeRef.current.value;
+        const totalStudent = totalStudentRef.current.value;
+
+        const newCourse = {image, title, price, totalClass, totalLecture, totalTime,totalStudent};
+
+        // post couse via API
+        fetch('https://',{
+            method : 'POST',
+            headers:{
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newCourse)
+        })
+            .then(res => res.json)
+            .then(data=>{
+                if(data.insertedId){
+                    alert('Successfully added the Course!')
+                    e.target.reset();
+                }
+            })
+    }
   return (
     <>
-       <Form style={{height:"520px"}}>
+       <Form style={{height:"520px"}}
+       onSubmit ={handleCourseForm}>
         <TextInput
             type="text"
             placeholder ="Enter Image Link"
             required
+            ref = {imageRef}
         />
         <TextInput
             type="text"
-            placeholder ="Enter Course Type"
+            placeholder ="Enter Course Title"
             required
-        />
-        <select />
-        <TextInput
-            type="text"
-            placeholder ="Enter Course Duration"
-            required
+            ref={titleRef}
         />
         <TextInput
             type="text"
             placeholder ="Enter Course Price"
             required
+            ref={priceRef}
         />
         <TextInput
             type="text"
-            placeholder ="Enter Course Image Link"
+            placeholder ="Enter Total Classes"
             required
+            ref = {totalClassRef}
         />
         <TextInput
             type="text"
-            placeholder ="Enter Course Category"
+            placeholder ="Enter Total Lecture Sheet"
             required
+            ref ={totalLectureRef}
         />
         <TextInput
             type="text"
-            placeholder ="Enter Course Instructor"
+            placeholder ="Enter Total Time to finish"
             required
+            ref = {totalTimeRef}
+        />
+        <TextInput
+            type="text"
+            placeholder ="Enter Total Enrolled Student"
+            required
+            ref = {totalStudentRef}
         />
          <button type="submit">ADD Course</button>
        </Form>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,7 +16,7 @@ export default function Nav() {
             <Link to='/'>Home</Link>
           </li>
           <li>
-            <Link to='/courses'>Services</Link>
+            <Link to='/courses'>Courses</Link>
           </li>
           <li>
             <Link to='/contact'>Contact Us</Link>
