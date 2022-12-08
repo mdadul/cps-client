b4772eadb67835d4048010b13c6521d1119971b6
Emdadul Islam <58410798+mdadul@users.noreply.github.com>
2022-12-08 03:21:59 +0000

    error fix

--- src/components/Instructor.js
+++ src/components/Instructor.js
@@ -10,18 +10,10 @@ export default function Instructor() {
   return (
     <div className={classes.card}>
       <img src={image} alt="John" style={{ width: "100%" }} />
-      <h1>Sorowar</h1>
-      <p className={classes.title}>CEO</p>
-      <p>IIUC</p>
+      <h1>Sorowar Mahbub</h1>
+      <p className={classes.title}>CEO, CPS</p>
+      <p>Dept. of CSE, IIUC</p>
       <SiFacebook/> <BsGithub/> <BsLinkedin/>
-     
-
-      {/* <Link href="#"><i className="fa fa-dribbble">facebook</i></Link>
-            <Link href="#"><i className="fa fa-twitter">Twitter</i></Link>
-            <Link href="#"><i className="fa fa-linkedin">Linkedin</i></Link> */}
-      <p>
-        <button>Contact</button>
-      </p>
     </div>
   );
 }
