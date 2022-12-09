77bd2069354f9797d4746151639d172fcc25e68b
Emdadul Islam <emdadulislam580@gmail.com>
2022-12-09 13:51:14 +0000

    Update InstrutorAddFrom.jsx and Courses.js

--- src/components/InstrutorAddFrom.jsx
+++ src/components/InstrutorAddFrom.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useRef } from "react";
 import Form from "./Form";
-
-// import TextInput from "./TextInput";
+import classes from "../stylesheet/TextInput.module.css";
+import { FcAddImage } from "react-icons/fc";
+import { BiRename } from "react-icons/bi";
+import { SiDesignernews } from "react-icons/si";
+import { MdCastForEducation } from "react-icons/md";
+import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
 
 export default function InstructorAddForm() {
   const imageRef = useRef();
@@ -53,48 +57,80 @@ export default function InstructorAddForm() {
   return (
     <>
       <Form style={{ height: "520px" }} onSubmit={handleMemberForm}>
-        <input
-          type="text"
-          placeholder="Enter Image Link"
-          required
-          ref={imageRef}
-        />
-        <input
-          type="text"
-          placeholder="Enter Name "
-          required
-          ref={nameRef}
-        />
-        <input
-          type="text"
-          placeholder="Enter Designation"
-          required
-          ref={designationRef}
-        />
-        <input
-          type="text"
-          placeholder="Enter Institutions"
-          required
-          ref={institutionsRef}
-        />
-        <input
-          type="text"
-          placeholder="Enter Facebook Profile 🔗"
-          required
-          ref={facebookRef}
-        />
-        <input
-          type="text"
-          placeholder="Enter Github 🔗"
-          required
-          ref={githubRef}
-        />
-        <input
-          type="text"
-          placeholder="Enter LinkedIn 🔗"
-          required
-          ref={linkedinRef}
-        />
+        <div className={classes.textInput}>
+          <input
+            type="text"
+            placeholder="Enter Image Link"
+            required
+            ref={imageRef}
+          />
+          <h2>
+            <FcAddImage />
+          </h2>
+        </div>
+        <div className={classes.textInput}>
+          <input type="text" placeholder="Enter Name " required ref={nameRef} />
+          <h2>
+            <BiRename />
+          </h2>
+        </div>
+        <div className={classes.textInput}>
+          <input
+            type="text"
+            placeholder="Enter Designation"
+            required
+            ref={designationRef}
+          />
+          <h2>
+            <SiDesignernews />
+          </h2>
+        </div>
+        <div className={classes.textInput}>
+          <input
+            type="text"
+            placeholder="Enter Institutions"
+            required
+            ref={institutionsRef}
+          />
+          <h2>
+            {" "}
+            <MdCastForEducation />{" "}
+          </h2>
+        </div>
+        <div className={classes.textInput}>
+          <input
+            type="text"
+            placeholder="Enter Facebook Profile 🔗"
+            required
+            ref={facebookRef}
+          />
+          <h2>
+            <BsFacebook />
+          </h2>
+        </div>
+        <div className={classes.textInput}>
+          <input
+            type="text"
+            placeholder="Enter Github 🔗"
+            required
+            ref={githubRef}
+          />
+          <h2>
+            <BsGithub />
+          </h2>
+        </div>
+        <div className={classes.textInput}>
+          <input
+            type="text"
+            placeholder="Enter LinkedIn 🔗"
+            required
+            ref={linkedinRef}
+          />
+          <h2>
+            <BsLinkedin />
+          </h2>
+        </div>
+
         <button type="submit">ADD Member</button>
       </Form>
     </>
