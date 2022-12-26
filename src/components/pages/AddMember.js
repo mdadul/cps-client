ebe8ee6dde1c5c3d2a01529f9d40b6f595401171
Emdadul Islam <emdadulislam580@gmail.com>
2022-12-26 16:59:11 +0000

    Update 9 files

--- src/components/pages/AddMember.js
+++ src/components/pages/AddMember.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { useEffect } from 'react'
-import Nav from '../Nav';
-import InstructorAddForm from '../InstrutorAddFrom';
-import Illustration from '../Illustration';
+import React from "react";
+import { useEffect } from "react";
+import Nav from "../Nav";
+import Footer from "../Footer";
+import InstructorAddForm from "../InstrutorAddFrom";
+import Illustration from "../Illustration";
 import add from "../../img/add.png";
 
+import Banner from "../Banner";
+
 export default function AddMember() {
-    useEffect(() => {
-        document.title = "Add Member-CPS";
-      }, []);
+  useEffect(() => {
+    document.title = "Add Member-CPS";
+  }, []);
   return (
     <>
-        <Nav></Nav>
-        <div className="container column">
+      <Nav></Nav>
+      <div className="container column">
         <InstructorAddForm />
         <Illustration src={add} />
       </div>
+      <Banner />
+      <Footer />
     </>
-  )
+  );
 }
